42738a97a4ec2f1ac0dabdba1928ea71481e266e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-06-17 00:04:31 +0500

    Параметры запроса

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/hi")
-def greet():
-    return "Hello, World!"
+def greet(who):
+    return f"Hello, {who}!"
 
 if __name__ == "__main__":
     import uvicorn
